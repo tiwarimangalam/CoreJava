db9633a1e781f051e4d9579b7c855b43b708cede
Mangalam Tiwari <mangalam.tiwari@zemosolabs.com>
2022-04-05 02:37:23 +0530

    All about variables in java

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        int i=0;
+        String s="Mangalam Tiwari";
+        float f=1;
+        boolean b=true;
+        short sh=0;
+        long l=1111111111;
+        char c='C';
+        System.out.println("int: "+i);
+        System.out.println("short: "+sh);
+        System.out.println("long: "+l);
+        System.out.println("char: "+c);
+        System.out.println("String: "+s);
+        System.out.println("boolean: "+b);
+    }
+}
