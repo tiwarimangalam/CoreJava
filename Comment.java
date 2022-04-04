1412eceb2eaacbf1af87415c60469665fc2de522
Mangalam Tiwari <mangalam.tiwari@zemosolabs.com>
2022-04-05 02:40:41 +0530

    Comment in java

---
+++ Comment.java
@@ -0,0 +1,7 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("This for comments");
+        System.out.println("Single line comment is denoted by: \n"+"// this is single line comment");
+        System.out.println("multiline line comment is denoted by: \n"+"/* this is multi line comment*/");
+    }
+}
