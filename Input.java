6eccd1b1e4802d7f068d6a84b38966ae5b6fd874
Mangalam Tiwari <mangalam.tiwari@zemosolabs.com>
2022-04-05 02:39:11 +0530

    All about taking the input

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public int takeInput() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        return n;
+        //System.out.println("The taken input is: " + n);
+    }
+}
