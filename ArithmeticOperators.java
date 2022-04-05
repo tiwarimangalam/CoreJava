d676188eb438816fec51198c64083ad4fff600ad
Mangalam Tiwari <mangalam.tiwari@zemosolabs.com>
2022-04-05 13:17:12 +0530

    Performed Arithmatic operations in java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a,b;
+        a=5;
+        b=2;
+        int result;
+        System.out.println("Addition: ");
+        result =a+b;
+        System.out.println(result);
+        System.out.println("Subtraction: ");
+        result =a-b;
+        System.out.println(result);
+        System.out.println("Multiplication: ");
+        result =a*b;
+        System.out.println(result);
+        System.out.println("Division: ");
+        result =a/b;
+        System.out.println(result);
+    }
+}
